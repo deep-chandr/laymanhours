6519ed020de216e1cf57c1c236973199ef3ede1b
deep <deep.dc216@gmail.com>
2018-08-30 02:07:56 +0530

    featured content

--- client/src/components/featuredPost/featuredPost.js
+++ client/src/components/featuredPost/featuredPost.js
@@ -5,6 +5,41 @@ import { Grid, Image, Header,Segment } from 'semantic-ui-react';
 import imgsmp from './1.png';
 
 class featuredPost extends Component{
+    state={
+        samplecontent: []
+    }
+    componentDidMount(){
+        const samplecontent = [
+            {
+                'mainHeading': 'padmavati',
+                'mainDate' : '27th Sept 18',
+                'mainTime' : '02:00 pm',
+                'content' : 'Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.',
+                'imgArr' : [imgsmp, imgsmp, imgsmp, imgsmp, imgsmp]
+            },{
+                'mainHeading': 'padmavati',
+                'mainDate' : '27th Sept 18',
+                'mainTime' : '02:00 pm',
+                'content' : 'Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.',
+                'imgArr' : [imgsmp, imgsmp, imgsmp, imgsmp, imgsmp]
+            },{
+                'mainHeading': 'padmavati',
+                'mainDate' : '27th Sept 18',
+                'mainTime' : '02:00 pm',
+                'content' : 'Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.',
+                'imgArr' : [imgsmp, imgsmp, imgsmp, imgsmp, imgsmp]
+            },{
+                'mainHeading': 'padmavati',
+                'mainDate' : '27th Sept 18',
+                'mainTime' : '02:00 pm',
+                'content' : 'Now that’s what I call stupid: In my junior year of high school, this guy asked me on a date. He rented a Redbox movie and made a pizza. We were watching the movie and the oven beeped so the pizza was done. He looked me dead in the eye and said, “This is the worst part.” I then watched this boy open the oven and pull the pizza out with his bare hands, rack and all, screaming at the top of his lungs. We never had a second date.',
+                'imgArr' : [imgsmp, imgsmp, imgsmp, imgsmp, imgsmp]
+            }
+        ]
+        this.setState({
+            samplecontent : samplecontent
+        })
+    }
     render(){
         return <div className={classes.topPost}>
             <Grid>
@@ -45,40 +80,46 @@ class featuredPost extends Component{
                         <div style={{ 'background-color': '', 'padding': '0 5%' }}>
                             <Grid doubling columns={2}>
                                 <Grid.Row stretched >
-                                    <Grid.Column  className={classes.main}>
-                                        <Grid doubling>
-                                            <Grid.Row stretched>
-                                                <Grid.Column width={11}>
-                                                    <Grid  columns={2} >
-                                                        <Grid.Row stretched>
-                                                        <Grid.Column>
-                                                            <img src={imgsmp} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
-                                                            <img src={imgsmp} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
+                                    {
+                                        this.state.samplecontent.map((val, i) => {
+                                            
+                                            return <Grid.Column  className={classes.main} style={{ 'padding': '0 5% 5% 5%' }}>
+                                                <Grid doubling>
+                                                    <Grid.Row stretched>
+                                                        <Grid.Column width={11} style={{ 'padding': '2%'}}>
+                                                            
+                                                            <Grid  columns={2} >
+                                                                <Grid.Row stretched>
+                                                                <Grid.Column style={{ 'padding': '0'}}>
+                                                                    <img src={val.imgArr[0]} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%', 'background-color': 'red'}} />
+                                                                    <img src={val.imgArr[1]} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
+                                                                </Grid.Column>
+                                                                <Grid.Column  style={{ 'padding': '0'}}>
+                                                                    <img src={val.imgArr[2]} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
+                                                                    <img src={val.imgArr[3]} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
+                                                                </Grid.Column>
+                                                                </Grid.Row>
+                                                            </Grid>
                                                         </Grid.Column>
-                                                        <Grid.Column>
-                                                            <img src={imgsmp} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
-                                                            <img src={imgsmp} style={{'height': '100%', 'object-fit': 'cover', 'width': '100%'}} />
+                                                        <Grid.Column width={5} style={{ 'padding': '0'}}>
+                                                            <img src={val.imgArr[4]} style={{ 'object-fit': 'cover', 'width': '100%'}} />
                                                         </Grid.Column>
-                                                        </Grid.Row>
-                                                    </Grid>
-                                                </Grid.Column>
-                                                <Grid.Column width={5}>
-                                                    <img src={imgsmp} style={{ 'object-fit': 'cover', 'width': '100%'}} />
-                                                </Grid.Column>
-                                            </Grid.Row>
-                                        </Grid>
-                                        {/* <img src={imgsmp} style={{'max-height': '400px', 'object-fit': 'cover', 'width': '100%'}} /> */}
-                                    </Grid.Column>
+                                                    </Grid.Row>
+                                                </Grid>
+                                            </Grid.Column>
+                                        })
+                                    }
+                                    
 
-                                    <Grid.Column  className={classes.main}>
+                                    {/* <Grid.Column  className={classes.main} style={{ 'padding': '0 5% 5% 5%' }}>
                                         <img src={imgsmp} style={{'max-height': '400px', 'object-fit': 'cover', 'width': '100%'}} />
                                     </Grid.Column>
-                                    <Grid.Column  className={classes.main}>
+                                    <Grid.Column  className={classes.main} style={{ 'padding': '0 5% 5% 5%' }}>
                                         <img src={imgsmp} style={{'max-height': '400px', 'object-fit': 'cover', 'width': '100%'}} />
                                     </Grid.Column>
-                                    <Grid.Column  className={classes.main}>
+                                    <Grid.Column  className={classes.main} style={{ 'padding': '0 5% 5% 5%' }}>
                                         <img src={imgsmp} style={{'max-height': '400px', 'object-fit': 'cover', 'width': '100%'}} />
-                                    </Grid.Column>
+                                    </Grid.Column> */}
                                 </Grid.Row>
                                 {/* <Grid.Row>
                                     <Grid.Column width={12} className={classes.main} style={{'background-color': 'red'}}>
